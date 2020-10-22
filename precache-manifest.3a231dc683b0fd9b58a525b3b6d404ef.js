74009ca6f7b735b0e093827af9a0fa5ccceb869c
Tajul Islam Rifat <mdr585643@gmail.com>
2020-10-22 03:12:44 -0400

    Updates

---
+++ precache-manifest.3a231dc683b0fd9b58a525b3b6d404ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5031f959104c840c4b93b2783e2e2d",
+    "url": "/react-redux-project/index.html"
+  },
+  {
+    "revision": "5c34ab69e4155cadf9a1",
+    "url": "/react-redux-project/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "94a86ee1d6d42192f9c8",
+    "url": "/react-redux-project/static/css/main.0e639d3f.chunk.css"
+  },
+  {
+    "revision": "5c34ab69e4155cadf9a1",
+    "url": "/react-redux-project/static/js/2.2461f434.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/react-redux-project/static/js/2.2461f434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a86ee1d6d42192f9c8",
+    "url": "/react-redux-project/static/js/main.dc6c8e08.chunk.js"
+  },
+  {
+    "revision": "799a1c58c42283088f29",
+    "url": "/react-redux-project/static/js/runtime-main.2f8c6f2a.js"
+  }
+]);
